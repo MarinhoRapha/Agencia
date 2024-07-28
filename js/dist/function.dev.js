b859636a07286f09b542a822520f7ece97222378
Marinho <158611550+MarinhoRapha@users.noreply.github.com>
2024-07-28 18:28:12 -0300

    Add files via upload

---
+++ js/dist/function.dev.js
@@ -0,0 +1,18 @@
+"use strict";
+
+$(function () {
+  // menu slide
+  $('nav.mobile').click(function () {
+    $(this).find('ul').slideToggle();
+  });
+  $('nav li, a').click(function () {
+    // script para pegar o atributo e descer 
+    var linkMenu = $(this).find('a');
+    var href = linkMenu.attr('href');
+    var descer = $(href).offset().top; // animação pra suavizar
+
+    $('html,body').animate({
+      'scrollTop': descer
+    });
+  });
+});
